3d4e62c5ce17de26dd6d74bd92501b1aebe40c65
HAMZA ALI KHAN <hamza1999alikhan@gmail.com>
2024-05-16 13:50:16 +0500

    Update albumRouter.js

--- src/routes/albumRouter.js
+++ src/routes/albumRouter.js
@@ -5,7 +5,7 @@ const albumController = require('../controllers/albumController');
 
 router.get('/', albumController.getAllAlbums);
 router.get('/:id', albumController.getAlbumById);
-router.post('/:id', albumController.getAlbumById);
+//router.post('/:id', albumController.getAlbumById);
 //router.post('/', albumController.createAlbum);
 //router.put('/:id', albumController.updateAlbum);
 //router.delete('/:id', albumController.deleteAlbum);
