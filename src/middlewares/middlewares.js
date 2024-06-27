59e76d8fda5167f1bbef7e931955fb033b0db974
HAMZA ALI KHAN <hamza1999alikhan@gmail.com>
2024-06-27 18:21:04 +0500

    Update middlewares.js

--- src/middlewares/middlewares.js
+++ src/middlewares/middlewares.js
@@ -2,5 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const app = express();
+
+// Middleware
 app.use(bodyParser.json());
 app.use(cors());
